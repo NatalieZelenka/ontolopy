4150c7405f57eae3be04ffb65a41bdbab9584a81
Natalie Thurlby <Natalie.thurlby@bristol.ac.uk>
2019-05-31 13:18:34 +0100

    building package

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
  
 long_description = open('README.md').read()
  
-setup(name='uberon-py', # name your package
-      packages=['uberon-py'], # same name as above
+setup(name='uberon_py', # name your package
+      packages=['uberon_py'], # same name as above
       version='0.0a1', 
-      description='`uberon-py` is a package for querying the Uberon ontology from python.',
+      description='`uberon_py` is a package for querying the Uberon ontology from python.',
       long_description=long_description,
       url='https://github.com/NatalieThurlby/uberon-py',
       author='Natlie Thurlby',
