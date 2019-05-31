77aed496a29c7b251df5e1c81713618d397c24f7
Natalie Thurlby <Natalie.thurlby@bristol.ac.uk>
2019-05-31 13:22:28 +0100

    Fixed setup.py to allow markdown description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='uberon_py', # name your package
       packages=['uberon_py'], # same name as above
       version='0.0a1', 
       description='`uberon_py` is a package for querying the Uberon ontology from python.',
+      long_description_content_type='text/markdown',
       long_description=long_description,
       url='https://github.com/NatalieThurlby/uberon-py',
       author='Natlie Thurlby',
