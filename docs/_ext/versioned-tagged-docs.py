d387b7f8e5fe47e3c240fd45d344e6b0235123a8
Natalie Thurlby <natalie.thurlby@gmail.com>
2021-03-28 19:47:44 +0100

    typo

--- docs/_ext/versioned-tagged-docs.py
+++ docs/_ext/versioned-tagged-docs.py
@@ -20,10 +20,10 @@ def copy_to_version(app, exception):
     try:
         branch_name = repo.active_branch.name
     except:
-        commit = repo.commit().hexsha
+        commit = repo.commit()
         tree = commit.tree
-        branch_name = repo.git.branch('--contains', commit).strip('* ')
-        logging.warning(f'Detached HEAD state, commit: {commit} on branch name {branch_name} tree {tree}.')
+        branch_name = repo.git.branch('--contains', commit.hexsha).strip('* ')
+        logging.warning(f'Detached HEAD state, commit: {commit.hexsha} on branch name {branch_name} tree {tree}.')
 
     ns = {}
     ver_path = convert_path(os.path.join(git_root, 'ontolopy/version.py'))
